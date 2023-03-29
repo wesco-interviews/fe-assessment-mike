ed9e296ad34ce3f273d847831f5e26df8da233d6
Mike Stecker <mike@stecker.me>
2023-03-29 14:15:38 -0700

    Grid cleanup

--- src/blocks/grid/grid.ts
+++ src/blocks/grid/grid.ts
@@ -12,30 +12,34 @@ function decorate({ items }: Grid) {
   const parent = document.createElement("div");
   parent.classList.add("grid");
 
-  items.forEach(function (item: GridItem) {
-    const { title, body, color } = item;
+  items.map(({ title, body, color }: GridItem) => {
     const itemDiv = document.createElement("div");
     itemDiv.classList.add("item");
 
     // Title
     const h1 = document.createElement("h1");
     h1.textContent = title;
-    itemDiv.append(h1);
+    itemDiv.appendChild(h1);
 
     // Body
     const content = document.createElement("p");
     content.textContent = body;
-    itemDiv.append(content);
+    itemDiv.appendChild(content);
 
     // Background color
     // Alternative: Can directly apply as background color
     // itemDiv.style.backgroundColor = color;
-    itemDiv.classList.add("item-" + color);
+    itemDiv.classList.add(`item-${color}`);
 
-    parent.append(itemDiv);
+    parent.appendChild(itemDiv);
   });
 
-  app?.append(parent);
+  // Add error handing
+  if (app) {
+    app.appendChild(parent);
+  } else {
+    console.error("Could not find element with id 'app'");
+  }
 }
 
 decorate(data);
